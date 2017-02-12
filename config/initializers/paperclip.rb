f2c27d0877104ca52bbd371fc062dfeea5661a71
Sung Jin Kim <sjkim0421@gmail.com>
2017-02-12 16:28:32 -0500

    add aws hostname

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,8 +1,15 @@
+if Rails.env.development?
+  bucket = ENV['AWS_DEV_BUCKET']
+else
+  bucket = ENV['AWS_PROD_BUCKET']
+end
+
 Paperclip::Attachment.default_options[:storage] = :s3
 Paperclip::Attachment.default_options[:s3_protocol] = 'http'
 Paperclip::Attachment.default_options[:s3_region] = ENV['AWS_REGION']
+Paperclip::Attachment.default_options[:s3_hostname] = ENV['AWS_HOST_NAME']
 Paperclip::Attachment.default_options[:s3_credentials] =
-  { :bucket => ENV['AWS_DEV_BUCKET'],
+  { :bucket => bucket,
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] }
 
