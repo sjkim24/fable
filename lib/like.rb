c43d5a1c5388b313c5ca9ff188bf1755488c1e22
Sung Jin Kim <sjkim0421@gmail.com>
2017-03-24 00:16:01 -0400

    delete lib/like.rb

--- lib/like.rb
+++
@@ -1,6 +0,0 @@
-module Like
-  def liked?(user_id, model, model_id) # two model_ids are story_id and comment_id
-    id_attr = model.concat("_id").to_sym
-    self.where(user_id: user_id, id_attr: model_id)
-  end
-end
