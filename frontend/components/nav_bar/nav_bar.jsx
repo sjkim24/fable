698f79f7fdeb8869c31545c45e449cc118cb152d
Sung Jin Kim <sjkim0421@gmail.com>
2017-03-24 19:09:56 -0400

    start on sliding menu

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,21 +1,66 @@
 import React, { Component } from "react";
 
 import NavBarLinks from "./nav_bar_links.jsx";
-// import HamburgerMenu from "../hamburger_menu.jsx";
 
 class NavBar extends Component {
+  constructor() {
+    super()
+    
+    this.state = { active: false };
+    this.toggleSlidingMenu = this.toggleSlidingMenu.bind(this);
+  }
+  
+  toggleSlidingMenu() {
+    this.setState({ active: !this.state.active });
+  }
+  
   render() {
-    // give active link a class to have the font color black
-
+    const state = this.state.active ? "active" : "hidden";
+    console.log(this.state.active);
     return(
       <nav className="nav-bar padding-side">
         <div className="nav-bar-inner group">
-          <img src="/images/icons/burger_b.png" alt="hamburger menu" className="nav-bar-hamburger-menu" />
+          <img 
+            src="/images/icons/burger_b.png" 
+            alt="hamburger menu" 
+            className="nav-bar-hamburger-menu" 
+            onClick={this.toggleSlidingMenu}/>
           <NavBarLinks />
+          <div className={`sliding-menu sliding-menu-${state}`}>
+            <ul className="sliding-menu-inner">
+              <li className="sliding-menu-item">
+                <a href="">Home</a>
+              </li>
+              <li className="sliding-menu-item">
+                <a href="">Top Stories</a>
+              </li>
+              <li className="sliding-menu-item">
+                <a href="">Politics</a>
+              </li>
+              <li className="sliding-menu-item">
+                <a href="">Technology</a>
+              </li>
+              <li className="sliding-menu-item">
+                <a href="">Humans</a>
+              </li>
+              <li className="sliding-menu-item">
+                <a href="">Culture</a>
+              </li>
+              <li className="sliding-menu-item">
+                <a href="">Business</a>
+              </li>
+              <li className="sliding-menu-item">
+                <a href="">Sports</a>
+              </li>
+              <li className="sliding-menu-item">
+                <a href="">Bookmark</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
   }
-}
+};
 
 export default NavBar;
