9661fd8cd3c6295cf6ac63f0e1d3782a10e4d3f9
Sung Jin Kim <sjkim0421@gmail.com>
2017-04-09 15:14:33 -0400

    remove binding.pry from sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,11 +22,9 @@ class SessionsController < Devise::SessionsController
   end
   
   def destroy
-    binding.pry
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
     set_flash_message! :notice, :signed_out if signed_out
     yield if block_given?
-    # respond_to_on_destroy
     
     render json: { authenticity_token: params[:authenticity_token] }
   end
