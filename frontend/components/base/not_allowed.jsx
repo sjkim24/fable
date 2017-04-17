73e5c6ed1569e4cf756d8d3c2eaaa537b1d1e264
Sung Jin Kim <sjkim0421@gmail.com>
2017-04-17 02:58:52 -0400

    stylze notallowed component

--- frontend/components/base/not_allowed.jsx
+++ frontend/components/base/not_allowed.jsx
@@ -4,7 +4,17 @@ class NotAllowed extends Component {
   render() {
     return (
       <div id="not-allowed">
-        You're not allowed here!
+        <div className="not-allowed-header">
+          You're not allowed here!
+        </div>
+        <a href="http://www.sjkim.io/PokeSnake/">
+          <img src="/images/pokeball.jpg" alt="pokeball image" className="not-allowed-pokeball blink" />
+        </a>
+        <div className="not-allowed-footer">
+          How about a little game instead?
+          <br></br>
+          Click the PokeBall...
+        </div>
       </div>
     );
   }
